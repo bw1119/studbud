881add8ff217afcb8f88999f646d16ebed8ca462
bw1119 <ggatwick121@gmail.com>
2022-04-07 19:56:58 +1000

    parcel init

--- public/server.js
+++ public/server.js
@@ -4,13 +4,16 @@ const res = require('express/lib/response');
 const app = express();
 
 // serve static files from public
-app.use(express.static('public'));
-
+app.use(express.static(__dirname + '/dist'));
+	res.sendFile(__dirname + '/dist/index.html')
+	
 // serve index file for root
 app.get('/',function(req,res) {
     res.sendFile(__dirname + '/public/index.html')
 });
 
+// init server on port 8888
 let server = app.listen(8888, function(){
     console.log("App server is running on port 8888");
 });
+
