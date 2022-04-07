7e481242f52b3c6464bb43e48a7bfcd493e12bd6
BW1119 <ggatwick121@gmail.com>
2022-04-07 18:22:12 +1000

    web server headers, basic web setup

---
+++ public/server.js
@@ -0,0 +1,16 @@
+// import + init express 
+const express = require('express');
+const res = require('express/lib/response');
+const app = express();
+
+// serve static files from public
+app.use(express.static('public'));
+
+// serve index file for root
+app.get('/',function(req,res) {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+let server = app.listen(8888, function(){
+    console.log("App server is running on port 8888");
+});
