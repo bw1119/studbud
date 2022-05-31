f2f260aa743926273a700e63970ec30098cd6c9d
bw1119 <ggatwick121@gmail.com>
2022-05-31 14:16:34 +1000

    initial colour pass and style changes; reorganised script

--- public/script.js
+++ public/script.js
@@ -56,34 +56,81 @@ document.addEventListener("DOMContentLoaded", function() {
   kanbanInit();
 });
 
+
+/////////////////////////
+// Handling kanban render functionality
+// - 
+/////////////////////////
+
 // Initialise board
 function kanbanInit(){
   // Called when page has been completely loaded
   window.addEventListener("load", function() {
     //flush();
     console.log(kanbanBoard);
+    
     // Creates a kanban column for every entry in the array
-    for (i=0; i < kanbanBoard.length; i++) {
+    for (let i=0; i < kanbanBoard.length; i++) {
       console.log('adding column ' + i + ', title:');
+      kanbanRenderColumn(i);
+    }; 
 
-      // Column div
-      let column = document.createElement('div',);
-
-      // Unnecessary as we use css selectors to select these divs
-      //column.setAttribute('class','kanban-column'); 
-
-      // Title taken from array
-      let columnTitle = document.createElement("h1");
-      columnTitle.textContent = kanbanBoard[i];
-      console.log('  ' + kanbanBoard[i]);
-
-      // Append column to board
-      board.appendChild(column);
-      // Append title to column
-      column.appendChild(columnTitle);
-    };
   });
 };
+
+function kanbanRenderColumn(i){
+  // Column div
+  let column = document.createElement('div',);
+  column.setAttribute('class','kanban-column'); 
+
+  // Title taken from array
+  let columnTitle = document.createElement("h1");
+  columnTitle.textContent = kanbanBoard[i];
+  console.log('  ' + kanbanBoard[i]);
+
+  // Append column to board
+  board.appendChild(column);
+  // Append title to column
+  column.appendChild(columnTitle);
+}
+
+// Add the new task to the html, with DOM
+// DEV: NEEDS REWRITE FOR KANBAN BOARD
+function renderTask(task){
+  // Create HTML elements
+  let item = document.createElement("li");
+  item.setAttribute('data-id', task.id);
+  item.innerHTML = "<p>" + task.taskTitle + "</p>";
+
+  // Append new item to task list on the document
+  taskList.appendChild(item);
+
+  // Extra Task DOM elements
+  let delButton = document.createElement("button");
+  let delButtonText = document.createTextNode("Delete Task");
+  delButton.appendChild(delButtonText);
+  item.appendChild(delButton);
+
+  // Event Listeners for DOM elements
+  delButton.addEventListener("click", function(event){
+    event.preventDefault();
+    let id = event.target.parentElement.getAttribute('data-id');
+    let index = taskListArray.findIndex(task => task.id === Number(id));
+    removeItemFromArray(taskListArray,index);
+    item.remove();
+  });
+
+  // Clear the input form
+  form.reset();
+}
+
+// Remove deleted task from array
+function removeItemFromArray(arr, index) {
+  if (index > -1){
+    arr.splice(index, 1);
+  }
+  return arr;
+}
 
 
 /////////////////////////
@@ -136,50 +183,13 @@ function addTask(taskTitle, dueDate, estimatedTimeMins, estimatedTimeHours, prio
   taskListArray.push(task);
   
   // DEV: log array list
-    console.log(taskListArray);
+  console.log(taskListArray);
   
   renderTask(task);
 };
 
+
 /////////////////////////
-// Handling general kanban render functionality
+// Handling general activetask section render functionality
 // - 
 /////////////////////////
-
-// Add the new task to the html, with DOM
-// DEV: NEEDS REWRITE FOR KANBAN BOARD
-function renderTask(task){
-  // Create HTML elements
-  let item = document.createElement("li");
-  item.setAttribute('data-id', task.id);
-  item.innerHTML = "<p>" + task.taskTitle + "</p>";
-
-  // Append new item to task list on the document
-  taskList.appendChild(item);
-
-  // Extra Task DOM elements
-  let delButton = document.createElement("button");
-  let delButtonText = document.createTextNode("Delete Task");
-  delButton.appendChild(delButtonText);
-  item.appendChild(delButton);
-
-  // Event Listeners for DOM elements
-  delButton.addEventListener("click", function(event){
-    event.preventDefault();
-    let id = event.target.parentElement.getAttribute('data-id');
-    let index = taskListArray.findIndex(task => task.id === Number(id));
-    removeItemFromArray(taskListArray,index);
-    item.remove();
-  });
-
-  // Clear the input form
-  form.reset();
-}
-
-// Remove deleted task from array
-function removeItemFromArray(arr, index) {
-  if (index > -1){
-    arr.splice(index, 1);
-  }
-  return arr;
-}
